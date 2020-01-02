9c8c173e59acb5ff61283b24f78604553b1788ab
Samip <samip.jungthapa@gmail.com>
2020-01-02 12:26:47 -0600

    Solution to Day 3 challenge

---
+++ Java Practice/Day 3 Practice/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N%2==1){System.out.println("Weird");}
+        else if((N>=2&&N<=5)&&N%2==0){System.out.println("Not Weird");}
+        else if((N>=6&&N<=20)&&N%2==0){System.out.println("Weird");}
+        else if ((N>20&&N<=100)&&N%2==0){System.out.println("Not Weird");}
+
+
+        scanner.close();
+    }
+}
